3a86f86c6dba4a80f485a9904b3ca652dea9762f
saeepatil-web <saeepatil532@gmail.com>
2025-11-22 15:49:03 +0530

    Implement energy storage logic for windmill operation

---
+++ code.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(){
+    int energy_stored;
+    printf("\n enter energy stored in percentage in the hydrogen power to gas(P2G):");
+    scanf("%d",&energy_stored);
+    if(energy_stored<=75){
+        printf("\nwindmill should be kept on for at least 2 hours");
+    }
+    
+    else if(energy_stored<=85){
+        printf("windmill to be kept on for at least 1 hour");
+
+    }
+    else if(energy_stored<=95){
+        printf("windmill to be kept on for at least 30 mins");
+
+    }
+    else if(energy_stored==100){
+        printf(“hydrogen power to gas(PG2) capacity to store energy is full hence windmill to be switched off");
+    }
+    else{
+        printf("enter agin");
+    }
+
+    
+    return 0;
+}
+        
